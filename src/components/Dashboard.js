96a3006fe4a2085c302efe3febf54468363cd639
Joseph Fajardo <jafajardo@gmail.com>
2018-06-12 10:59:19 +1000

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import proptypes from 'prop-types';
+
+export default class Dashboard extends Component {
+  render() {
+    return(
+      <div>Dashboard</div>
+    );
+  }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { browserHistory } from 'react-router';
 
-import Home from './home';
+import Dashboard from './Dashboard';
 import Page1 from './page1';
 
 const App = () => (
   <Router history={browserHistory}>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Dashboard} />
       <Route path="/Page1" component={Page1} />
     </Switch>
   </Router>
